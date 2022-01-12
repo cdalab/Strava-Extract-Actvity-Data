628b7620fd25eabfdbd2f5932567e0c0f2e15a5a
MaorSagi <maors802@gmail.com>
2022-01-12 22:35:09 +0200

    extract analysis pages - bug fixed (if csv not exist)

--- link_extractor.py
+++ link_extractor.py
@@ -140,8 +140,7 @@ class LinksExtractor(Browser):
             log(f'Failed fetching rider activity links, current rider fetched {rider_id}', 'ERROR',
                 id=self.id)
 
-
-    def _validate_activity_page(self,activity_soup,rider_id,activity_id):
+    def _validate_activity_page(self, activity_soup, rider_id, activity_id):
         heading = activity_soup.find('section', attrs={'id': 'heading'})
         rider_in_page = heading.find(
             lambda tag: (tag.name == "a") and (
@@ -151,11 +150,10 @@ class LinksExtractor(Browser):
         if (rider_in_page is None) or (rider_in_page != float(rider_id)):
             raise ValueError(
                 f'The cyclist {rider_id} mapped to activity {activity_id}, but the real rider for this activity is {rider_in_page}')
-        units_in_page = [u['title'] for u in heading.find_all('abbr',attrs={'class':'unit'})]
+        units_in_page = [u['title'] for u in heading.find_all('abbr', attrs={'class': 'unit'})]
         if any([unit in units_in_page for unit in UNDESIRED_UNITS]):
             raise ValueError(
                 f'The metrics in the page of activity {activity_id}, cyclist {rider_id}, are wrong.')
-
 
     def _activity_analysis_links(self, rider_id, csv_file_path):
         try:
@@ -168,12 +166,14 @@ class LinksExtractor(Browser):
                 html_content = read_from_html(f"{rider_dir_path}/{activity_id}", 'overview.html')
                 activity_soup = BeautifulSoup(html_content, 'html.parser')
                 menu_options = activity_soup.find('nav', attrs={'class': 'sidenav'}).find_all('a')
-                self._validate_activity_page(activity_soup,rider_id,activity_id)
+                self._validate_activity_page(activity_soup, rider_id, activity_id)
                 for option in menu_options:
                     ref = option.attrs['href']
                     activity_option_link = f"{BASE_STRAVA_URL}{ref}"
                     option_type = ref.split(f"{activity_id}")[-1]
-                    if (option_type in OPTIONS_TO_IGNORE) or (activity_option_link in pd.read_csv(csv_file_path)['activity_option_link'].values):
+                    csv_exist = os.path.exists(csv_file_path)
+                    if (option_type in OPTIONS_TO_IGNORE) or (csv_exist and (
+                            activity_option_link in pd.read_csv(csv_file_path)['activity_option_link'].values)):
                         continue
                     row = {'rider_id': rider_id,
                            'activity_id': activity_id,
