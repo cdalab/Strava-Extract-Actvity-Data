88db8ab8f8eebbd91e4dd04d22ee5fbd0d1381b3
MaorSagi <maors802@gmail.com>
2022-01-13 16:07:09 +0200

    add activity type - "Virtual Ride"

--- consts.py
+++ consts.py
@@ -12,7 +12,7 @@ ACTIVITY_POST_TYPES = ["Activity", "GroupActivity", "ChallengeJoin", "ClubJoin"]
 ANALYSIS_PAGE_TYPES = ['/analysis', '/power-curve', '/zone-distribution',
                        '/power-distribution', '/est-power-curve',
                        '/est-power-distribution', '/heartrate']
-ACTIVITY_TYPES = ['Ride','Indoor Cycling']
+ACTIVITY_TYPES = ['Ride', 'Indoor Cycling', 'Virtual Ride']
 
 USERS = [
     r'kvspek@gmail.com',
