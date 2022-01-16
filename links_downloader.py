776e47f56539ab9a8fe489baf965acf97f1df209
MaorSagi <maors802@gmail.com>
2022-01-17 01:19:13 +0200

    rider has not add weight case - bug fixed

--- links_downloader.py
+++ links_downloader.py
@@ -231,9 +231,9 @@ class LinksDownloader(Browser):
         if watts_metric_activity == watts_kg_metric_activity:
             raise ValueError(
                 f'The watts/kg curve has not loaded yet, activity: {rider_activity["activity_id"]}')
-        cyclist_has_weight = WebDriverWait(self.browser, 2).until(
+        cyclist_hasnt_add_weight = WebDriverWait(self.browser, 2).until(
             EC.presence_of_all_elements_located((By.XPATH, '//*[@id="power-panel-cpcurve"]/div')))[-1]
-        if 'none' not in cyclist_has_weight.get_attribute('style'):
+        if cyclist_hasnt_add_weight.is_displyed():
             return {f"{rider_activity['option_type'][1:]}_watts": watts_metric_activity}
         return {f"{rider_activity['option_type'][1:]}_watts": watts_metric_activity,
                 f"{rider_activity['option_type'][1:]}_watts-kg": watts_kg_metric_activity}
