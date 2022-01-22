53148547482053be555a74d3a98b7736927cbfb5
MaorSagi <maors802@gmail.com>
2022-01-22 11:40:18 +0200

    remove '--no-sandbox' option

--- browser.py
+++ browser.py
@@ -121,7 +121,7 @@ class Browser:
 
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
-        options.add_argument('--no-sandbox')
+        # options.add_argument('--no-sandbox')
         options.add_argument('log-level=1')
         self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
