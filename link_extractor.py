33940af96a4b8628f1fd0349a953029f3775361b
MaorSagi <maors802@gmail.com>
2022-01-23 00:27:40 +0200

    update of start/end week option

--- link_extractor.py
+++ link_extractor.py
@@ -1,5 +1,5 @@
 from urllib.parse import parse_qsl
-
+import shutil
 import pandas as pd
 from bs4 import BeautifulSoup
 from utils import *
@@ -31,12 +31,17 @@ class LinksExtractor(Browser):
                 url_param_dict = dict(parse_qsl(link.split('?')[1]))
                 interval = url_param_dict['interval']
                 curr_year, curr_week = int(interval[:4]), int(interval[4:])
-                start_year, s_week = int(start_week[:4]), int(start_week[4:])
-                end_year, e_week = int(end_week[:4]), int(end_week[4:])
-                if (start_year <= curr_year <= end_year) and (s_week <= curr_week <= e_week):
-                    row = {'rider_id': rider_id,
-                           'time_interval_link': link}
-                    append_row_to_csv(csv_file_path, row)
+                if start_week is not None:
+                    start_year, s_week = int(start_week[:4]), int(start_week[4:])
+                    if (curr_year < start_year) or (curr_week < s_week):
+                        continue
+                if end_week is not None:
+                    end_year, e_week = int(end_week[:4]), int(end_week[4:])
+                    if (curr_year >= end_year) or (curr_week >= e_week):
+                        continue
+                row = {'rider_id': rider_id,
+                        'time_interval_link': link}
+                append_row_to_csv(csv_file_path, row)
         except:
             log(f'Could not fetch year interval links for rider {rider_id}.', 'ERROR', id=self.id)
 
@@ -120,10 +125,10 @@ class LinksExtractor(Browser):
                             activity_a = activity_a[0]
                         else:
                             entries = activity_card.find(lambda tag: (tag.name == 'ul') and ('class' in tag.attrs) and (
-                                        'GroupActivity--list-entries' in str(tag.attrs['class'])))
+                                    'GroupActivity--list-entries' in str(tag.attrs['class'])))
                             for entry in entries.contents:
                                 link = entry.find(lambda tag: tag.name == 'a' and (
-                                            ('athletes' in tag.attrs['href']) or ('pros' in tag.attrs['href'])))['href']
+                                        ('athletes' in tag.attrs['href']) or ('pros' in tag.attrs['href'])))['href']
                                 if float(link.split('/')[-1]) == float(rider_id):
                                     activity_a = entry.find(
                                         lambda tag: tag.name == "a" and "/activities/" in tag.attrs['href'])
@@ -241,3 +246,208 @@ class LinksExtractor(Browser):
         except:
             log(f'Failed fetching rider activity links, current rider fetched {rider_id}', 'ERROR',
                 id=self.id)
+
+    def _fetch_activity_analysis_data(self, rider_id):
+        try:
+            rider_dir_path = f"{self.html_files_path}/{rider_id}"
+            rider_activity_dirs = os.listdir(rider_dir_path)
+            i = 0
+            for activity_id in rider_activity_dirs:
+                log(f"Fetching activity data from file {rider_dir_path}/{activity_id}, {i} / {len(rider_activity_dirs) - 1}",
+                    id=self.id, debug=False)
+                activity_dir_path = f"{rider_dir_path}/{activity_id}"
+                rider_activity_files = os.listdir(activity_dir_path)
+                for activity_file in rider_activity_files:
+                    if os.path.isdir(f"{activity_dir_path}/{activity_file}"):
+                        continue
+                    activity_link = f"{BASE_STRAVA_URL}/activities/{activity_id}"
+                    html_content = read_from_html(activity_dir_path, activity_file)
+                    soup = BeautifulSoup(html_content, 'html.parser')
+                    args=(activity_file, activity_link, rider_id, activity_id)
+                    if 'overview' in activity_file:
+                        self._handle_overview_page(soup, *args)
+                    elif 'analysis' in activity_file:
+                        self._handle_analysis_page(soup, *args)
+                    elif 'power-curve' in activity_file:
+                        self._handle_power_curve_page(soup, *args)
+                    elif ('heartrate' in activity_file) or ('zone' in activity_file):
+                        self._handle_table_page(soup, *args)
+                    elif 'power-distribution' in activity_file:
+                        self._handle_power_distribution_page(soup, *args)
+                    else:
+                        log(f'Could not fetch analysis data for activity file {rider_dir_path}/{activity_id}/{activity_file}, unknown type page.',
+                            'ERROR', id=self.id)
+                print_progress_bar(i + 1, len(rider_activity_dirs), prefix='Progress:', suffix='Complete',
+                                   length=50)
+                i += 1
+        except:
+            log(f'Could not fetch analysis data for rider {rider_id}.', 'ERROR', id=self.id)
+
+    def extract_data_from_analysis_activities(self):
+        try:
+            rider_id = None
+            i = 0
+            for rider_id in self.pages:
+                log(f'Fetching activity analysis data for cyclist {rider_id}, {i} / {len(self.pages) - 1}',
+                    id=self.id)
+                self._fetch_activity_analysis_data(rider_id)
+                i += 1
+
+        except:
+            log(f'Failed fetching rider activity links, current rider fetched {rider_id}', 'ERROR',
+                id=self.id)
+
+    def _handle_overview_table(self, overview_soup, file, activity_link, rider_id, activity_id):
+        args = (file, activity_link, rider_id, activity_id)
+        overview_table = overview_soup.find('div', attrs={'class': 'spans8 activity-stats mt-md mb-md'})
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - overview table is empty or doesn't exist."
+        if self._element_doesnt_exist(((overview_table is None) or (len(overview_table.contents) == 0)), warn_msg,
+                                      *args):
+            return
+        ul_table = overview_table.find_all('ul')
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - overview table ul's missing."
+        if self._element_doesnt_exist((len(ul_table) == 0), warn_msg, *args):
+            return
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - overview table li's missing."
+        for ul in ul_table:
+            if self._element_doesnt_exist(((len(ul.contents) == 0) or (len(ul.find_all('li')) < 2)), warn_msg, *args):
+                return
+        div_table = overview_table.findChildren("div", recursive=False)
+        # warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - overview table div's missing."
+        # if self._element_doesnt_exist((len(div_table) == 0), warn_msg, *args):
+        #     return
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - overview table div's contents missing."
+        for div in div_table:
+            if self._element_doesnt_exist((len(div.contents) == 0), warn_msg, *args):
+                return
+
+    def _handle_overview_page(self, soup, *args):
+        self._handle_overview_table(soup, *args)
+        title = soup.find('section', attrs={'id': 'heading'}).find('span', attrs={
+            'class': 'title'}).text.replace('\n', '')
+        deli_idx = title.find('–')
+        activity_type = title[deli_idx + 1:].strip() if deli_idx > 0 else None
+        if activity_type == 'Indoor Cycling':
+            self._handle_analysis_stacked_chart(soup, *args)
+
+    def _handle_power_curve_page(self, file, soup, activity_link, rider_id, activity_id):
+        pass
+
+
+    def _section_and_header_handler(self, soup, file, activity_link, rider_id, activity_id):
+        args = (file, activity_link, rider_id, activity_id)
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - section part is missing."
+        section = soup.find('div',attrs={'id':'view'}).find('section', attrs={'class': 'with-border'})
+        if self._element_doesnt_exist(((section is None) or (len(section.contents) == 0)), warn_msg, *args):
+            return
+        header = section.find('header')
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - header is missing."
+        if self._element_doesnt_exist(((header is None) or (len(header.contents) == 0)), warn_msg, *args):
+            return
+        uls_header = header.findChildren('ul')
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - ul's in header are missing."
+        if self._element_doesnt_exist(((uls_header is None) or (len(uls_header) == 0)), warn_msg, *args):
+            return
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - li's in ul header are missing."
+        for ul in uls_header:
+            if self._element_doesnt_exist(len(ul.contents) == 0, warn_msg, *args):
+                return
+        return section
+
+
+    def _handle_table_page(self, soup, file, activity_link, rider_id, activity_id):
+        args = (file, activity_link, rider_id, activity_id)
+        section = self._section_and_header_handler(soup, *args)
+        if section is None:
+            return
+        table = section.find("table")
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - table is missing."
+        if self._element_doesnt_exist(((table is None) or (len(table.contents) == 0)), warn_msg, *args):
+            return
+        tbody = table.find("tbody")
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - tbody is missing."
+        if self._element_doesnt_exist(((tbody is None) or (len(tbody.contents) == 0)), warn_msg, *args):
+            return
+        trs = tbody.findChildren("tr")
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - trs are missing."
+        if self._element_doesnt_exist((len(trs) == 0), warn_msg, *args):
+            return
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - tds are missing."
+        for tr in trs:
+            tds = tr.findChildren("td")
+            if self._element_doesnt_exist((len(tds) == 0), warn_msg, *args):
+                return
+
+
+    def _handle_power_distribution_page(self, file, soup, activity_link, rider_id, activity_id):
+        pass
+
+    def _handle_analysis_page(self, soup,*args):
+        self._handle_analysis_stacked_chart(soup, *args)
+        self._handle_elevation_chart(soup, *args)
+
+    def _handle_base_chart_and_svg(self, soup, file, activity_link, rider_id, activity_id):
+        args = (file, activity_link, rider_id, activity_id)
+        base_chart = soup.find('div', attrs={'class': 'base-chart'})
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - base chart is missing."
+        if self._element_doesnt_exist(((base_chart is None) or (len(base_chart.contents) == 0)), warn_msg, *args):
+            return
+        svg = base_chart.find('svg')
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - svg is missing."
+        if self._element_doesnt_exist(((svg is None) or (len(svg.contents) == 0)), warn_msg, *args):
+            return
+        return svg
+
+    def _handle_analysis_stacked_chart(self, soup, file, activity_link, rider_id, activity_id):
+        stacked_chart = soup.find('div', attrs={'id': 'stacked-chart'})
+        args = (file, activity_link, rider_id, activity_id)
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - stacked chart is missing."
+        if self._element_doesnt_exist(stacked_chart is None, warn_msg, *args):
+            return
+        svg = self._handle_base_chart_and_svg(stacked_chart, *args)
+        if svg is None:
+            return
+        paths = svg.find_all('path', attrs={'class': 'simple-line'})
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - paths are missing."
+        if self._element_doesnt_exist(len(paths) == 0, warn_msg, *args):
+            return
+        lines = stacked_chart.find_all(lambda tag: (tag.name == 'g') and ('clip-path' in tag.attrs))
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - paths or lines are missing."
+        if self._element_doesnt_exist(len(paths) != len(lines), warn_msg, *args):
+            return
+        boxes = stacked_chart.find_all('g', attrs={'class': 'label-box'})
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - boxes are missing."
+        if self._element_doesnt_exist(len(boxes) != len(paths), warn_msg, *args):
+            return
+
+    def _save_activity_page_to_download_again(self, file, activity_link, rider_id, activity_id):
+        src_dir = f"{self.html_files_path}/{rider_id}/{activity_id}"
+        Path(f'{src_dir}/backup').mkdir(parents=True, exist_ok=True)
+        shutil.move(f"{src_dir}/{file}", f"{src_dir}/backup/{file}")
+        csv_exists = os.path.exists(DOWNLOAD_AGAIN_FILE_PATH)
+        if (not csv_exists) or (activity_link not in pd.read_csv(DOWNLOAD_AGAIN_FILE_PATH)['activity_link'].values):
+            activity_of_overview = {'rider_id': rider_id,
+                                    'activity_link': activity_link,
+                                    'activity_id': activity_id}
+            append_row_to_csv(DOWNLOAD_AGAIN_FILE_PATH, activity_of_overview)
+
+    def _handle_elevation_chart(self, soup, file, activity_link, rider_id, activity_id):
+        elevation_chart = soup.find('div', attrs={'id': 'elevation-chart'})
+        args = (file, activity_link, rider_id, activity_id)
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - stacked chart is missing."
+        if self._element_doesnt_exist(elevation_chart is None, warn_msg, *args):
+            return
+        svg = self._handle_base_chart_and_svg(elevation_chart, *args)
+        if svg is None:
+            return
+        paths = svg.find_all('path', attrs={'class': 'area-chart-curr'})
+        warn_msg = f"Activity {activity_id} of rider {rider_id} should be downloaded again - paths are missing."
+        if self._element_doesnt_exist(len(paths) == 0, warn_msg, *args):
+            return
+
+    def _element_doesnt_exist(self, pred, msg, *args):
+        if pred:
+            self._save_activity_page_to_download_again(*args)
+            log(msg, "WARNING", id=self.id)
+            return True
+        return False
