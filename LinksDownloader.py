da409a9df1fa16f0bc3b1efb44704065788e72f2
MaorSagi <maors802@gmail.com>
2022-01-30 17:13:03 +0200

    refactoring and fixing firsts scripts to renew downloads

--- LinksDownloader.py
+++ LinksDownloader.py
@@ -80,7 +80,7 @@ class LinksDownloader(Browser):
         response = self._is_valid_html()
         if response is not None:
             return response
-        t.sleep(random.random() + 0.5)
+        t.sleep(random.random() + 0.5 + random.randint(2, 3))
         current_interval_range_element = WebDriverWait(self.browser, 3).until(
             EC.presence_of_element_located((By.ID, "interval-value")))
         current_interval_range = current_interval_range_element.text
